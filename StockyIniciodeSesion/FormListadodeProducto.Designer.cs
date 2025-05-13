8c9b075bb96e77f0bbff033e91fdc80c37e1844f
Neblan06 <montielperezjuandiego38@gmail.com>
2025-05-13 13:56:36 -0600

    cambio13

---
+++ StockyIniciodeSesion/FormListadodeProducto.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace StockyIniciodeSesion
+{
+    partial class FormListadodeProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgvListadoProducto = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UltimaEntrada = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvListadoProducto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgvListadoProducto
+            // 
+            this.dtgvListadoProducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvListadoProducto.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nombre,
+            this.Precio,
+            this.Cantidad,
+            this.UltimaEntrada});
+            this.dtgvListadoProducto.Location = new System.Drawing.Point(31, 66);
+            this.dtgvListadoProducto.Name = "dtgvListadoProducto";
+            this.dtgvListadoProducto.Size = new System.Drawing.Size(546, 316);
+            this.dtgvListadoProducto.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "IDProductos";
+            this.ID.Name = "ID";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "NombreProducto";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "PrecioProducto";
+            this.Precio.Name = "Precio";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "CantidadPrducto";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // UltimaEntrada
+            // 
+            this.UltimaEntrada.HeaderText = "UltimoProducto";
+            this.UltimaEntrada.Name = "UltimaEntrada";
+            // 
+            // FormListadodeProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtgvListadoProducto);
+            this.Name = "FormListadodeProducto";
+            this.Text = "FormListadodeProducto";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvListadoProducto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvListadoProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UltimaEntrada;
+    }
+}
